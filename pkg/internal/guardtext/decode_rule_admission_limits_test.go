30c2aa0a87f79a7ddee2fcc9c7375df1bd22f75c
park285 <pjoonwoo111@gmail.com>
2026-07-22 04:01:27 +0900

    test(promptguard): validate rule-owned expansion bounds

--- pkg/internal/guardtext/decode_rule_admission_limits_test.go
+++ pkg/internal/guardtext/decode_rule_admission_limits_test.go
@@ -5,11 +5,7 @@ import "testing"
 func TestRuleCandidateExpansionSharesProtectedContextBudget(t *testing.T) {
 	t.Parallel()
 
-	decoder := contextDecoder{
-		protectedWork: protectedDecodeWork{
-			contextBytes: maxProtectedContextBytes,
-		},
-	}
+	decoder := ruleContextDecoder{expansionBytes: maxProtectedContextBytes}
 	if decoder.ruleCandidateMayExpand("YQ==Wdub3Jl", "aWdub3Jl") {
 		t.Fatal("expansion analysis exceeded the protected context budget")
 	}
@@ -21,9 +17,11 @@ func TestRuleCandidateExpansionSharesProtectedContextBudget(t *testing.T) {
 func TestRuleExpansionQueueIsBounded(t *testing.T) {
 	t.Parallel()
 
-	decoder := contextDecoder{
-		queue:   make([]decodeQueueEntry, maxDecodeScans),
-		visited: make(map[string]struct{}),
+	decoder := ruleContextDecoder{
+		contextDecoder: contextDecoder{
+			queue:   make([]decodeQueueEntry, maxDecodeScans),
+			visited: make(map[string]struct{}),
+		},
 	}
 	decoder.deferRuleCandidate(decodeQueueEntry{}, "ordinary readable candidate")
 	if decoder.result.Status&DecodeScanLimit == 0 {
